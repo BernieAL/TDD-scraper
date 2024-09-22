dde842404fb2273bbd18c5dbb1de269af66b2238
ubuntu <ubuntu@ubuntu50gb.myguest.virtualbox.org>
2024-09-22 16:59:01 -0400

    built out scraped vs db stored  comparison functionality

--- Analysis/compare_data.py
+++ Analysis/compare_data.py
@@ -40,74 +40,79 @@ for e in existing_product_ids_and_prices:
     existing_product_id_prices_dict[product_id] = last_price
 
 
-# print(product_ids)
+#2 - read in scraped data and compare these to db product_ids to scraped product_ids
+updated_products = []
+new_products = []
 
-# #2 - read in scraped data
-# #compare these to db product_ids to scraped product_ids
-# scraped_data = []
-# with open(test_input_file,mode='r') as file:
+
+scraped_data = []
+with open(test_input_file,mode='r') as file:
     
-#     #standard csv reader to skip first lines (meta data) which is csv scrape data and query info
-#     csv_reader = csv.reader(file)
+    #standard csv reader to skip first lines (meta data) which is csv scrape data and query info
+    csv_reader = csv.reader(file)
 
-#     #get scrape data from first line
-#     scrape_date_line = next(csv_reader)
-#     scrape_date = scrape_date_line[0].split(':')[1].strip()
-#     scrape_date = datetime.strptime(scrape_date,'%Y-%d-%m').date()
-#     print("Scrape Date:", scrape_date)
+    #get scrape data from first line
+    scrape_date_line = next(csv_reader)
+    scrape_date = scrape_date_line[0].split(':')[1].strip()
+    scrape_date = datetime.strptime(scrape_date,'%Y-%d-%m').date()
+    print("Scrape Date:", scrape_date)
 
-#     #get query from second line
-#     query_line = next(csv_reader)
-#     query = query_line[0].split(':')[1].strip()
-#     print("Query:",query)
+    #get query from second line
+    query_line = next(csv_reader)
+    query = query_line[0].split(':')[1].strip()
+    print("Query:",query)
 
-#     #switch to csv.DictReader for rest of row for reading product data
-#     csv_reader = csv.DictReader(file)
-#     # Iterate over each row and print product data
-#     for row in csv_reader:
-#         print(f"Brand: {row['Brand']}, Product: {row['Product_Name']}, Price: {row['Price']}, ID: {row['Product_ID']}")
+    #switch to csv.DictReader for rest of row for reading product data
+    csv_reader = csv.DictReader(file)
+    # Iterate over each row and print product data
+    for row in csv_reader:
+        print(f"Brand: {row['Brand']}, Product: {row['Product_Name']}, Price: {row['Price']}, ID: {row['Product_ID']}")
 
-#         #check if scraped product is in db
-#         if row['Product_ID'] in existing_product_ids_and_prices:
+        #check if scraped product is in db
+        if row['Product_ID'] in existing_product_id_prices_dict:
+            print(f"product found in db")
+            #if yes, check if any price changes -> compare scraped price to db price
+            if row['Price'] != existing_product_id_prices_dict[product_id]:
+                
+                #if yes, push product to update_products array with new price to updated in db later
+                temp = {
+                    'product_id':row['product_id'],
+                    'last_scrape_date': 'scrape_date',
+                    'last_price':row['Price'],
+                }
+                updated_products.push(temp)
 
-#             #if yes, check if any price changes -> compare scraped price to db price
-#             if row['Price'] != existing_product_ids_and_prices['product-id':['Product']]:
-#                 pass
-#                 #if yes, push product to update_products array with new price to updated in db later
+                #mark as seen by popping 'seen' product from db dictionary - what remains are db items thaat are no longer on website anymore - meaning they are sold
+                existing_product_id_prices_dict.pop(product_id)
 
-#                 #mark as seen by popping 'seen' product from db dictionary - what remains are db items thaat are no longer on website anymore - meaning they are sold
+            #only update the last_scrape date
+            else:
+                temp = {
+                    'product_id':row['product_id'],
+                    'last_scrape_date': 'scrape_date',
+                }
+                updated_products.push(temp)
 
+        else:
+            temp = {
+                'product_id':row['product_id'],
+                'brand':row['Brand'],
+                # 'Product_name':row['Product_Name'],
+                'last_scrape_date': 'scrape_date',
+                'last_price':row['Price'],
+            }
+            #if no, push product to new_products array to be bulk inserted to db later
+            new_products.push(temp)
 
-#             #if no, push product to new_products array to be bulk inserted to db later
-
-
-
-
-# #3 - compare new data to existing
-# new_products = []
-# updated_products = []
-
-# for product in scraped_data:
-#     product_id = product['Product_ID']
-#     print(product_id)
-
-#     if product_id in existing_product_ids:
-        
-#         #check for price changes by comparing existing to newly scraped
-#         if price_difference(product_id,product['Price']):
-#             #if item still available, remove from existing_product_ids, what remains will be items that were not found today - indicating they were sold
-#             updated_products.append(product)
-#         existing_product_ids.remove(product_id)
-#     else:
-#         #new product
-#         new_products.append(product)
+            
 
 
-# #4 - bulk insert new products
 
-# #5 - bulk update price changes
+#4 - bulk insert new products
 
-# #6 - mark items that remain in existing_product_ids as sold
+#5 - bulk update price changes
+
+#6 - mark items that remain in existing_product_ids as sold
 
         
 
