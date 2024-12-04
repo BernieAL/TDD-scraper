b4498582111079c549625783447f68afbb454468
ubuntu <ubuntu@ubuntu50gb.myguest.virtualbox.org>
2024-12-04 17:42:27 -0500

    resolved rbmq connection issue for .env.local - host name issue

--- config/connections.py
+++ config/connections.py
@@ -1,7 +1,7 @@
 import pika
 import os
 
-RABBITMQ_HOST = os.getenv('RABBITMQ_HOST','localhost')
+RABBITMQ_HOST = os.getenv('RABBITMQ_HOST')
 
 def get_rabbitmq_connection_params():
     return pika.ConnectionParameters(
@@ -14,3 +14,9 @@ def get_rabbitmq_connection_params():
 def create_rabbitmq_connection():
     #creates new rbmq connnection using std params
     return pika.BlockingConnection(get_rabbitmq_connection_params())
+
+
+
+
+if __name__ == "__main__":
+    create_rabbitmq_connection()
